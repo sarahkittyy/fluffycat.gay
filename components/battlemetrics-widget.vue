0d28f19b0e2b97fcdf8609b0b5963acfa1eda0ce
Sarah Ohlin <sarahohlin1@gmail.com>
2023-10-31 23:53:09 -0400

    updated battlemetrics banner

--- components/battlemetrics-widget.vue
+++ components/battlemetrics-widget.vue
@@ -1,3 +1,3 @@
 <template>
-	<a href="https://www.battlemetrics.com/servers/tf2/24249758"><img src="https://cdn.battlemetrics.com/b/standardVertical/24249758.png?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3A24H&chartColor=%23FF0700&maxPlayersHeight=300" /></a>
+	<a href="https://www.battlemetrics.com/servers/tf2/24366374"><img src="https://cdn.battlemetrics.com/b/standardVertical/24366374.png?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%23222222&chart=players%3A24H&chartColor=%23FF0700&maxPlayersHeight=300" /></a>
 </template>
